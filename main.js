3d0f3867d008990810b52013ac0a02d6629b2e41
JUNSEOK-AHN <134159813+JUNSEOK-AHN@users.noreply.github.com>
2024-04-04 04:41:52 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,22 +2,20 @@ const textArea = document.querySelector('#textArea');
 const btn = document.querySelector("#btn");
 const textDiv = document.querySelector("#textDiv");
 const generateBtn = document.querySelector('#generateBtn');
-/*
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-*/
+const selectAll = document.querySelector('#selectAll');
+const selectAllBtn = document.querySelector('#selectAllBtn');
+const addTerm = document.querySelector('#addTerm');
+const addTermBtn = document.querySelector('#addTermBtn');
 
 textArea.value = '';
+selectAll.value = '';
+addTerm.value = '';
 
 textArea.addEventListener('paste', () => {
   if (textArea.value !== '') {
     textArea.value += '\n';
   }
-});
-
+})
 
 btn.addEventListener("click", () => {
   textDiv.innerText = textArea.value;
@@ -34,7 +32,6 @@ btn.addEventListener("click", () => {
     }
   };
 
-  
 
   tempArrayBox1.forEach((sentence) => {    
     let x = sentence;
@@ -46,24 +43,12 @@ btn.addEventListener("click", () => {
     x = x.replaceAll('"', ' " ');
     
     tempArrayBox2.push(x);
-    /*
-    const sentenceLength = sentence.length;
-    const lastWord = sentence.charAt(sentenceLength - 1)
-
-    if (lastWord === '.') {
-      const x = sentence.replaceAll(".", " .");
-      tempArrayBox2.push(x);
-    } else {
-      tempArrayBox2.push(sentence);
-    };
-    */
   });
 
 
   let tempArrayBox3 = [];
 
 
-  // // // // // // // 
 
   for (let i = 0; i < tempArrayBox2.length; i++) {
     let eachWorldArray = tempArrayBox2[i].split(' ');
@@ -120,44 +105,41 @@ btn.addEventListener("click", () => {
         textDiv.removeChild(elem);
     } 
 
-
-
-
-
-
-
-
   });
 
   const eachWord = document.getElementsByClassName("word");
 
   [...eachWord].forEach((word) => {
     word.addEventListener('click', () => {
-      word.classList.toggle('selected');
-      
       let wordDataOrder = word.getAttribute("data-order");
       let wordTerm = word.textContent;
-
       const allTermSpans = [...document.getElementsByClassName("term")];
 
-      allTermSpans.forEach(span => { 
-        const termDataOrder = span.getAttribute("data-order");
-        if (wordDataOrder === termDataOrder) {
-          span.setAttribute("data-term", wordTerm);
-        }
-      })
-
+      if (!word.classList.contains('selected')) {
+        word.classList.add('selected');
+        allTermSpans.forEach(span => { 
+          const termDataOrder = span.getAttribute("data-order");
+          if (wordDataOrder === termDataOrder) {
+            span.setAttribute("data-term", wordTerm);
+          }
+        })
+      } else {
+        word.classList.remove('selected');
+        allTermSpans.forEach(span => { 
+          const termDataOrder = span.getAttribute("data-order");
+          if (wordDataOrder === termDataOrder) {
+            span.setAttribute("data-term", "");
+          }
+        })
+      }
+      // word.classList.toggle('selected');
     })
+
   });
 });
-/*
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-*/
-generateBtn.addEventListener('click', () => {
+
+
+const generateFx = () => {
   const selectedWord = document.getElementsByClassName('selected');
 
   [...selectedWord].forEach(elem => {
@@ -168,10 +150,60 @@ generateBtn.addEventListener('click', () => {
 
   [...terms].forEach(elem => {
     let term = elem.getAttribute('data-term');
-    // let replacedTerm = term.replaceAll(/[`~!@#$%^&*()_|+\=?;:'",.<>\{\}\[\]\\\/]/gi, "");
     elem.textContent = term;
-    // elem.textContent = replacedTerm;
   });
+
+  [...terms].forEach(elem => {
+    let term = elem.getAttribute('data-term');
+    if (term === '') {
+      elem.textContent = 'EMPTY'
+      elem.classList.add('notSelected');
+    } else {
+      elem.classList.remove('notSelected')
+    }
+  })
+
+};
+
+
+generateBtn.addEventListener('click', () => {
+  generateFx();
+})
+
+selectAllBtn.addEventListener('click', () => {
+  const whichWord = selectAll.value;
+  const whichWordArr = whichWord.split(', ');
+  const words = document.getElementsByClassName('word');
+  
+  [...words].forEach((word) => {
+    for (let i = 0; i < whichWordArr.length; i++) {
+      if (word.textContent.toLowerCase() === whichWordArr[i]) {
+        let wordDataOrder = word.getAttribute("data-order");
+        let wordTerm = word.textContent;
+        const allTermSpans = [...document.getElementsByClassName("term")];
+        
+        word.classList.add('selected')
+        allTermSpans.forEach(span => { 
+          const termDataOrder = span.getAttribute("data-order");
+          if (wordDataOrder === termDataOrder) {
+            span.setAttribute("data-term", wordTerm);
+          }
+        })
+      }
+    }
+  });
+  generateFx();
 })
 
 
+addTermBtn.addEventListener('click', () => {
+  const allTermSpans = [...document.getElementsByClassName("term")];
+  const additionStr = addTerm.value;
+
+  console.log(allTermSpans);
+  console.log(additionStr);
+
+  allTermSpans.forEach(elem => {
+    elem.textContent += ` ${additionStr}`;
+  });
+})
